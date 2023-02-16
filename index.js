f99ab42629b43ef3662ebda84d3c334e27f235c2
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2023-02-15 21:55:39 -0300

    mysql + sequelize, tabelas postagens e usuarios

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const express = require("express");
 const app = express()
 
-//rotas
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/html/index.html")
-})
-
-app.get("/sobre", function(req,res){
-    res.sendFile(__dirname + "/html/sobre.html")
-})
-
-app.get("/blog", function(req,res){
-    res.send("Bem vindo ao meu blog")
-})
-
-app.get("/ola/:cargo/:nome/:cor", function(req, res){
-    res.send(`<h1>Ola ${req.params.nome}!</h1> <h2>Seu cargo é: ${req.params.cargo}</h2> <h2>Sua cor favorita é: ${req.params.cor}`)
-})
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081")
---
+++ teste.sql
@@ -0,0 +1,1 @@
+UPDATE usuarios SET nome = "Matheus Mendes" WHERE nome = "matheus Mendes"
