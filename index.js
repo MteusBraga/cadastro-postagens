366ea880e0dc0e2701be81d02ffc3e1e22a62fea
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2023-02-15 22:21:39 -0300

    views folder + rota e formulario

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require("sequelize")
+
+
 
 //Config
     //Template Engine
-    app.engine('handlebars', handlebars({default: 'main'}))
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+    //Conexao com o banco de dados MySQL
+    const sequelize = new Sequelize('sistemadecadastro', 'root', '1964', {
+        host: 'localhost',
+        dialect:'mysql'
+    })
+//Rotas
+    app.get('/cad', function(req,res){
+        res.render('layouts/formulario')
+    })
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081")
-}) //ultima linha
-//funcao de callback é uma funcao que é utilizada sempre que tal evento é disparado
+}) 
