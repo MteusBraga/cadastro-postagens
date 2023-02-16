7252f637d0f92b3519f8020dd36a5e61131d1ab4
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2023-02-16 00:15:19 -0300

    create, delete, list com sequelize

--- index.js
+++ index.js
@@ -2,25 +2,50 @@ const express = require("express");
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-
+const Post = require('./models/Post')
 
 //Config
     //Template Engine
-    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main',  runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+  
+        allowProtoMethodsByDefault: true,
+      }}))
     app.set('view engine', 'handlebars')
     //Body Parser
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
     
 //Rotas
+
+    app.get('/', function(req, res){
+        Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
+            res.render('layouts/home', {posts: posts})
+        })
+    })
     app.get('/cad', function(req,res){
         res.render('layouts/formulario')
     })
 
     app.post('/add', function(req,res){
-        res.send(`Texto: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send('Houve um erro: '+erro)
+        })
     })
 
-app.listen(8081, function(){
-    console.log("Servidor Rodando na url http://localhost:8081")
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(){
+            res.send('Postagem deletada com sucesso')
+        }).catch(function(erro){
+            res.send('Esta postagem nao existe')
+        })
+    })
+
+app.listen(8082, function(){
+    console.log("Servidor Rodando na url http://localhost:8082")
 }) 
